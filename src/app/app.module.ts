573c8f9de205849d82b873c238ef5833d4913c66
Mauricio <cesalcedo1@poligran.edu.co>
2020-09-24 14:20:40 -0500

    Actualizacion pie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { PieComponent } from './pie/pie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    
-    CuerpoComponent
+    CuerpoComponent,
+    PieComponent
   ],
   imports: [
     BrowserModule,
