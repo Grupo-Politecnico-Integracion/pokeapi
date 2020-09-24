24f1a3151d2d20a7e514f27f1325281ec12bc396
Mauricio <cesalcedo1@poligran.edu.co>
2020-09-24 10:13:50 -0500

    actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabeceraComponent
+    CabeceraComponent,
+    CuerpoComponent
   ],
   imports: [
     BrowserModule,
