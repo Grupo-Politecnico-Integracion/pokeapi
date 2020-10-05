f994d73a3030172a945af71b89353d70a3ca99b6
Mauricio <cesalcedo1@poligran.edu.co>
2020-10-04 19:32:06 -0500

    Nuevo componente

---
+++ src/app/especificacion/especificacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-especificacion',
+  templateUrl: './especificacion.component.html',
+  styleUrls: ['./especificacion.component.scss']
+})
+export class EspecificacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
