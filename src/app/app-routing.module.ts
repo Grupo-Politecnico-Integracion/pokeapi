e9162392b7327922b7a655d039e44a3f78f1d05d
Mauricio <cesalcedo1@poligran.edu.co>
2020-10-06 08:00:38 -0500

    update rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { EspecificacionComponent } from './especificacion/especificacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '', component: CuerpoComponent
+  },
+  {
+    path: 'especificacion', component: EspecificacionComponent
+  },
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
